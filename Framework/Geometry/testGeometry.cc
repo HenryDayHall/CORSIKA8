f567b2ef1afd95d35cb6717c218782b5fa59cdc3
ralfulrich <ralf.ulrich@kit.edu>
2018-08-09 09:55:38 +0200

    first unit testing examples

---
+++ Framework/Geometry/testGeometry.cc
@@ -0,0 +1,26 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <ThirdParty/catch2/catch.hpp>
+
+#include <Units/PhysicalUnits.h>
+
+using namespace phys::units;
+using namespace phys::units::io;
+using namespace phys::units::literals;
+
+TEST_CASE( "PhysicalUnits", "[Units]" )
+{  
+  SECTION( "sectionOne" )
+    {
+      REQUIRE( 1_m/1_m == 1 );
+    }
+  
+  SECTION( "sectionTwo" )
+    {
+      REQUIRE_FALSE( 1_m/1_m == 2 );
+    }
+
+  SECTION( "sectionThree" )
+    {
+      REQUIRE( 1_s/1_s == 2 );
+    }
+}
