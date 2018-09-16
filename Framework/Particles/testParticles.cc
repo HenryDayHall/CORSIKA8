be092ceae9c7a4ff0be7acea0bfe616b3bf649f8
ralfulrich <ralf.ulrich@kit.edu>
2018-09-17 00:16:27 +0200

    a bit beeter and more complete tests for particle properties

--- Framework/Particles/testParticles.cc
+++ Framework/Particles/testParticles.cc
@@ -9,16 +9,39 @@
 using namespace corsika::units;
 using namespace corsika::particles;
 
-TEST_CASE("Particles", "[Particles]") {
+TEST_CASE("ParticleProperties", "[Particles]") {
 
-  SECTION("Types") { REQUIRE(Electron::GetCode() == Code::Electron); }
+  SECTION("Types") {
+    REQUIRE(Electron::GetCode() == Code::Electron);
+    REQUIRE(Positron::GetCode() == Code::Positron);
+    REQUIRE(Proton::GetCode() == Code::Proton);
+    REQUIRE(Neutron::GetCode() == Code::Neutron);
+    REQUIRE(Gamma::GetCode() == Code::Gamma);
+    REQUIRE(PiPlus::GetCode() == Code::PiPlus);
+  }
 
-  SECTION("Data") {
+  SECTION("Masses") {
     REQUIRE(Electron::GetMass() / (511_keV / constants::cSquared) == Approx(1));
     REQUIRE(Electron::GetMass() / GetMass(Code::Electron) == Approx(1));
+  }
+
+  SECTION("Charges") {
     REQUIRE(Electron::GetCharge() / constants::e == Approx(-1));
     REQUIRE(Positron::GetCharge() / constants::e == Approx(+1));
     REQUIRE(GetElectricCharge(Positron::GetAntiParticle()) / constants::e == Approx(-1));
-    REQUIRE(Electron::GetName() == "e-");
   }
+
+  SECTION("Names") {
+    REQUIRE(Electron::GetName() == "e-");
+    REQUIRE(PiMinus::GetName() == "pi-");
+  }
+
+  SECTION("PDG") {
+    REQUIRE(GetPDG(Code::PiPlus) == 211);
+    REQUIRE(GetPDG(Code::DPlus) == 411); 
+    REQUIRE(GetPDG(Code::NuMu) == 14); 
+    REQUIRE(GetPDG(Code::NuE) == 12); 
+    REQUIRE(GetPDG(Code::MuMinus) == 13); 
+  }
+  
 }
