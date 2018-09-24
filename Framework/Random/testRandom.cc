570a026668355e166e722bef6f36ed5e9b66cd54
Maximilian Reininghaus <maximilian.reininghaus@kit.edu>
2018-09-24 13:29:03 +0200

    basic interface for RNG

---
+++ Framework/Random/testRandom.cc
@@ -0,0 +1,26 @@
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one
+                          // cpp file
+#include <catch2/catch.hpp>
+
+#include <corsika/random/RNGManager.h>
+#include <iostream>
+
+using namespace corsika::random;
+
+SCENARIO("random-number streams can be registered and retrieved") {
+  GIVEN("a RNGManager") {
+    RNGManager rngManager;
+
+    WHEN("a sequence is registered by name") {
+      rngManager.RegisterRandomStream("stream_A");
+
+      THEN("the sequence can be retrieved") {
+        REQUIRE_NOTHROW(rngManager.GetRandomStream("stream_A"));
+      }
+
+      THEN("an unknown sequence cannot be retrieved") {
+        REQUIRE_THROWS(rngManager.GetRandomStream("stream_UNKNOWN"));
+      }
+    }
+  }
+}
