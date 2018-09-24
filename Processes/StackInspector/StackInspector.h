7ada8afc69c4ef6e53bb09377e807739a09c2049
ralfulrich <ralf.ulrich@kit.edu>
2018-09-24 15:46:57 +0200

    added stackinsprector module

---
+++ Processes/StackInspector/StackInspector.h
@@ -0,0 +1,23 @@
+#ifndef _Physics_StackInspector_StackInspector_h_
+#define _Physics_StackInspector_StackInspector_h_
+
+namespace corsika::process {
+
+  namespace stack_inspector {
+
+    class StackInspector {
+
+    public:
+      StackInspector();
+      ~StackInspector();
+
+      void init();
+      void run();
+      double GetStepLength();
+    };
+
+  } // namespace null_model
+
+} // namespace corsika::process
+
+#endif
