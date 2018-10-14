7dd04aa4ca5c33ce81e3b06408aa19869fe598c6
ralfulrich <ralf.ulrich@kit.edu>
2018-10-14 23:23:03 +0200

    clang-format

--- Processes/StackInspector/StackInspector.h
+++ Processes/StackInspector/StackInspector.h
@@ -1,13 +1,13 @@
 
 /**
-  * (c) Copyright 2018 CORSIKA Project, corsika-project@lists.kit.edu
-  * 
-  * See file AUTHORS for a list of contributors.
-  * 
-  * This software is distributed under the terms of the GNU General Public
-  * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
-  * the license.
-*/
+ * (c) Copyright 2018 CORSIKA Project, corsika-project@lists.kit.edu
+ *
+ * See file AUTHORS for a list of contributors.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
+ * the license.
+ */
 
 #ifndef _Physics_StackInspector_StackInspector_h_
 #define _Physics_StackInspector_StackInspector_h_
