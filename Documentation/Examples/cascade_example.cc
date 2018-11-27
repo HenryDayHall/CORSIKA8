00796aabbcd3acdc7f4a1dc6a150d6c13b96eb28
Felix Riehn <felix@matilda>
2018-11-27 16:59:01 +0000

    added cascade example with sibyll, primitive

--- Documentation/Examples/cascade_example.cc
+++ Documentation/Examples/cascade_example.cc
@@ -24,6 +24,7 @@
 #include <corsika/process/sibyll/ParticleConversion.h>
 
 #include <corsika/units/PhysicalUnits.h>
+
 using namespace corsika;
 using namespace corsika::process;
 using namespace corsika::units;
