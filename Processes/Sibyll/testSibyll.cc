91b7043fe9bf0baeade48df2e53be7b92bec6f94
ralfulrich <ralf.ulrich@kit.edu>
2018-11-30 07:59:14 +0100

    fixed the canInteract, knownby flags for sibyll, and added the xs-type code

--- Processes/Sibyll/testSibyll.cc
+++ Processes/Sibyll/testSibyll.cc
@@ -17,6 +17,8 @@
                           // cpp file
 #include <catch2/catch.hpp>
 
+#include <iostream>
+using namespace std;
 using namespace corsika;
 
 TEST_CASE("Sibyll", "[processes]") {
@@ -30,7 +32,7 @@ TEST_CASE("Sibyll", "[processes]") {
     REQUIRE(process::sibyll::ConvertToSibyll(corsika::particles::Electron::GetCode()) ==
             process::sibyll::SibyllCode::Electron);
     REQUIRE(process::sibyll::ConvertToSibyllRaw(corsika::particles::Proton::GetCode()) ==
-            13 );
+            13);
   }
 
   SECTION("KnownBySibyll") {
@@ -41,12 +43,19 @@ TEST_CASE("Sibyll", "[processes]") {
   }
 
   SECTION("canInteractInSibyll") {
-    REQUIRE(process::sibyll::CanInteract(corsika::particles::Proton::GetCode()));
-    REQUIRE(process::sibyll::CanInteract(corsika::particles::XiCPlus::GetCode()));
 
-    REQUIRE_FALSE(
-		  process::sibyll::CanInteract(corsika::particles::Electron::GetCode()));
-    REQUIRE_FALSE(
-		  process::sibyll::CanInteract(corsika::particles::SigmaC0::GetCode()));
+    REQUIRE(process::sibyll::CanInteract(corsika::particles::Proton::GetCode()));
+    REQUIRE(process::sibyll::CanInteract(corsika::particles::Code::XiCPlus));
+
+    REQUIRE_FALSE(process::sibyll::CanInteract(corsika::particles::Electron::GetCode()));
+    REQUIRE_FALSE(process::sibyll::CanInteract(corsika::particles::SigmaC0::GetCode()));
+  }
+  
+  SECTION("cross-section type") {
+
+    REQUIRE(process::sibyll::GetSibyllXSCode(corsika::particles::Code::Electron) == 0);
+    REQUIRE(process::sibyll::GetSibyllXSCode(corsika::particles::Code::K0Long) == 3);
+    REQUIRE(process::sibyll::GetSibyllXSCode(corsika::particles::Code::SigmaPlus) == 1);
+    REQUIRE(process::sibyll::GetSibyllXSCode(corsika::particles::Code::PiMinus) == 2);
   }
 }
