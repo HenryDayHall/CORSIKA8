ece530f9f0a5e35ff066b931a3e910cd57e93aad
Felix Riehn <felix@matilda>
2018-12-02 11:05:34 +0000

    fixed rebase

--- Framework/Units/PhysicalUnits.h
+++ Framework/Units/PhysicalUnits.h
@@ -37,15 +37,6 @@ namespace corsika::units::si {
   using EnergyType = phys::units::quantity<phys::units::energy_d, double>;
   using MassType = phys::units::quantity<phys::units::mass_d, double>;
   using MassDensityType = phys::units::quantity<phys::units::mass_density_d, double>;
-
-  // defining momentum you suckers
-  // dimensions, i.e. composition in base SI dimensions
-  using momentum_d                     = phys::units::dimensions< 1, 1, -1 >;
-  // defining the unit of momentum, so far newton-meter, maybe go to HEP?
-  constexpr phys::units::quantity< momentum_d > newton_second   { meter * kilogram / second };
-  // defining the type
-
-  using MomentumType = phys::units::quantity<momentum_d, double>;
   using CrossSectionType = phys::units::quantity<sigma_d, double>;
 
 } // end namespace corsika::units::si
