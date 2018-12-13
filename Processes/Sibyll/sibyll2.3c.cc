dfc2b7eb7b9cd558d0e7f652a414452c905c81b4
ralfulrich <ralf.ulrich@kit.edu>
2018-12-13 06:56:03 +0100

    finisehd move sibyll

---
+++ Processes/Sibyll/sibyll2.3c.cc
@@ -0,0 +1,11 @@
+#include <corsika/process/sibyll/sibyll2.3c.h>
+
+#include <corsika/random/RNGManager.h>
+
+
+double s_rndm_(int&) {
+  static corsika::random::RNG& rmng =
+      corsika::random::RNGManager::GetInstance().GetRandomStream("s_rndm");
+  ;
+  return rmng() / (double)rmng.max();
+}
