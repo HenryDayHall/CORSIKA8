246145361668a505d0568fd67c38e2300493ef2d
Felix Riehn <felix@matilda>
2018-12-29 14:32:28 +0000

    added field for Environment to Sibyll process

--- Documentation/Examples/cascade_example.cc
+++ Documentation/Examples/cascade_example.cc
@@ -223,7 +223,7 @@ int main() {
   // setup processes, decays and interactions
   tracking_line::TrackingLine<setup::Stack> tracking(env);
   stack_inspector::StackInspector<setup::Stack> p0(true);
-  corsika::process::sibyll::Interaction sibyll;
+  corsika::process::sibyll::Interaction sibyll(env);
   corsika::process::sibyll::Decay decay;
   ProcessCut cut(8_GeV);
   corsika::process::TrackWriter::TrackWriter trackWriter("tracks.dat");
