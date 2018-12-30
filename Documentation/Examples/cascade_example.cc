41349dbd771be7b00f897d14ad42741568e60adf
Felix Riehn <felix@matilda>
2018-12-30 19:45:08 +0000

    moved target sampling from sibyll process to medium

--- Documentation/Examples/cascade_example.cc
+++ Documentation/Examples/cascade_example.cc
@@ -272,6 +272,8 @@ int main() {
 
   cout << "Result: E0=" << E0 / 1_GeV << endl;
   cut.ShowResults();
+  const HEPEnergyType Efinal = cut.GetCutEnergy() + cut.GetInvEnergy() + cut.GetEmEnergy();
   cout << "total energy (GeV): "
-       << (cut.GetCutEnergy() + cut.GetInvEnergy() + cut.GetEmEnergy()) / 1_GeV << endl;
+       << Efinal / 1_GeV << endl
+       << "relative difference (%): " << (Efinal / E0 - 1. ) * 100 << endl;
 }
