a1556502d020e3d6a1e2693951e8998e9ab74acf
ralfulrich <ralf.ulrich@kit.edu>
2019-01-14 14:48:41 +0100

    also allow reference members in FourVector

--- Processes/Sibyll/Decay.h
+++ Processes/Sibyll/Decay.h
@@ -167,6 +167,11 @@ namespace corsika::process {
         using corsika::geometry::Point;
         using namespace corsika::units::si;
 
+        // TODO: this should be done in a central, common place. Not here..
+#ifndef CORSIKA_OSX
+        feenableexcept(FE_INVALID);
+#endif
+
         fCount++;
         SibStack ss;
         ss.Clear();
@@ -212,6 +217,11 @@ namespace corsika::process {
         }
         // empty sibyll stack
         ss.Clear();
+
+        // TODO: this should be done in a central, common place. Not here..
+#ifndef CORSIKA_OSX
+        fedisableexcept(FE_INVALID);
+#endif
       }
     };
   } // namespace sibyll
