15e05fb26399d1d6a7757489afbca7614f3547c8
Felix Riehn <felix@matilda>
2019-01-24 18:53:18 +0000

    implemented DoInteraction for nuclei

--- Documentation/Examples/cascade_example.cc
+++ Documentation/Examples/cascade_example.cc
@@ -124,6 +124,14 @@ public:
         is_inv = true;
         break;
 
+      case Code::Neutron:
+        is_inv = true;
+        break;
+
+      case Code::AntiNeutron:
+        is_inv = true;
+        break;
+
       default:
         break;
     }
@@ -234,33 +242,39 @@ int main() {
   stack_inspector::StackInspector<setup::Stack> p0(true);
 
   corsika::random::RNGManager::GetInstance().RegisterRandomStream("s_rndm");
-  //  corsika::process::sibyll::Interaction sibyll(env);
-  corsika::process::sibyll::NuclearInteraction sibyll(env);
+  corsika::process::sibyll::Interaction sibyll(env);
+  corsika::process::sibyll::NuclearInteraction sibyllNuc(env);
   corsika::process::sibyll::Decay decay;
   ProcessCut cut(8_GeV);
 
-  corsika::random::RNGManager::GetInstance().RegisterRandomStream("HadronicElasticModel");
-  corsika::process::HadronicElasticModel::HadronicElasticInteraction hadronicElastic(env);
+  // corsika::random::RNGManager::GetInstance().RegisterRandomStream("HadronicElasticModel");
+  // corsika::process::HadronicElasticModel::HadronicElasticInteraction hadronicElastic(env);
 
   corsika::process::TrackWriter::TrackWriter trackWriter("tracks.dat");
 
   // assemble all processes into an ordered process list
-  auto sequence = p0 << sibyll << decay << hadronicElastic << cut << trackWriter;
-
+  //auto sequence = p0 << sibyll << decay << hadronicElastic << cut << trackWriter;
+  auto sequence = p0 << sibyll << sibyllNuc << decay << cut << trackWriter;
+  
   // cout << "decltype(sequence)=" << type_id_with_cvr<decltype(sequence)>().pretty_name()
   // << "\n";
 
   // setup particle stack, and add primary particle
   setup::Stack stack;
   stack.Clear();
-  const HEPEnergyType E0 =
-      100_TeV; // 1_PeV crashes with bad COMboost in second interaction (crash later)
+  const Code beamCode = Code::Carbon;
+  const HEPEnergyType E0 = 100_TeV;
+    //      100_TeV; // 1_PeV crashes with bad COMboost in second interaction (crash later)
   double theta = 0.;
   double phi = 0.;
+  
   {
     auto particle = stack.NewParticle();
-    particle.SetPID(Code::Helium);
-    HEPMomentumType P0 = sqrt(E0 * E0 - Helium::GetMass() * Helium::GetMass());
+    particle.SetPID(beamCode);
+    auto elab2plab = []( HEPEnergyType Elab, HEPMassType m){
+		       return sqrt(Elab * Elab - m * m);
+		     };
+    HEPMomentumType P0 = elab2plab( E0, corsika::particles::GetMass( beamCode ) );
     auto momentumComponents = [](double theta, double phi, HEPMomentumType ptot) {
       return std::make_tuple(ptot * sin(theta) * cos(phi), ptot * sin(theta) * sin(phi),
                              -ptot * cos(theta));
