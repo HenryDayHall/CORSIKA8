61411184312cf01494dbd3f68b8bc8437d7668ee
ralfulrich <ralf.ulrich@kit.edu>
2019-01-31 14:44:20 +0100

    update copyrights...

--- Framework/Random/ExponentialDistribution.h
+++ Framework/Random/ExponentialDistribution.h
@@ -10,27 +10,6 @@
  */
 
 
-/*
- * (c) Copyright 2018 CORSIKA Project, corsika-project@lists.kit.edu
- *
- * See file AUTHORS for a list of contributors.
- *
- * This software is distributed under the terms of the GNU General Public
- * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
- * the license.
- */
-
-
-/*
- * (c) Copyright 2018 CORSIKA Project, corsika-project@lists.kit.edu
- *
- * See file AUTHORS for a list of contributors.
- *
- * This software is distributed under the terms of the GNU General Public
- * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
- * the license.
- */
-
 #ifndef _include_ExponentialDistribution_h
 #define _include_ExponentialDistribution_h
 
--- Processes/TrackWriter/TrackWriter.cc
+++ Processes/TrackWriter/TrackWriter.cc
@@ -10,16 +10,6 @@
  */
 
 
-/*
- * (c) Copyright 2018 CORSIKA Project, corsika-project@lists.kit.edu
- *
- * See file AUTHORS for a list of contributors.
- *
- * This software is distributed under the terms of the GNU General Public
- * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
- * the license.
- */
-
 #include <corsika/process/track_writer/TrackWriter.h>
 #include <string>
 
