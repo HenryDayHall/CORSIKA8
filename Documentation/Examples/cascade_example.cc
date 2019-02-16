6552cb6ee403215d2b7281c4df3077f7f8651152
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2019-02-16 14:36:38 +0100

    include <set> header

--- Documentation/Examples/cascade_example.cc
+++ Documentation/Examples/cascade_example.cc
@@ -42,6 +42,7 @@ using boost::typeindex::type_id_with_cvr;
 #include <iostream>
 #include <limits>
 #include <typeinfo>
+#include <set>
 
 using namespace corsika;
 using namespace corsika::process;
