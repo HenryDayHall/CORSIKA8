73da63d0eb1e76bb413ea614a5e8e986b91f547f
ralfulrich <ralf.ulrich@kit.edu>
2019-02-22 23:03:49 +0100

    simplified a bit. have to complify later.

--- Documentation/Examples/cascade_example.cc
+++ Documentation/Examples/cascade_example.cc
@@ -277,8 +277,8 @@ int main() {
   // assemble all processes into an ordered process list
   // auto sequence = stackInspect << sibyll << decay << hadronicElastic << cut << trackWriter;
   // auto sequence = stackInspect << sibyll << sibyllNuc << decay << eLoss << cut << trackWriter;
-  auto sequence = sibyll << sibyllNuc << decay << eLoss << cut;
-  // auto sequence = stackInspect << sibyll << sibyllNuc << decay << cut << trackWriter;
+  //auto sequence = sibyll << sibyllNuc << decay << eLoss << cut;
+  auto sequence = stackInspect << sibyll << sibyllNuc << decay << eLoss << cut;
 
   // cout << "decltype(sequence)=" << type_id_with_cvr<decltype(sequence)>().pretty_name()
   // << "\n";
