d845d1016865dc3ea8b76ad643665993d5e55d76
felix@home <felix.riehn@kit.edu>
2019-05-07 22:59:17 +0100

    added ParticleCut proess

---
+++ Processes/ParticleCut/testParticleCut.cc
@@ -0,0 +1,72 @@
+
+/*
+ * (c) Copyright 2018 CORSIKA Project, corsika-project@lists.kit.edu
+ *
+ * See file AUTHORS for a list of contributors.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
+ * the license.
+ */
+
+#include <corsika/process/particle_cut/ParticleCut.h>
+
+#include <corsika/environment/Environment.h>
+#include <corsika/geometry/Point.h>
+#include <corsika/geometry/RootCoordinateSystem.h>
+#include <corsika/geometry/Vector.h>
+#include <corsika/units/PhysicalUnits.h>
+#include <corsika/utl/CorsikaFenv.h>
+
+#include <corsika/setup/SetupStack.h>
+
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one
+                          // cpp file
+#include <catch2/catch.hpp>
+
+using namespace corsika;
+using namespace corsika::process::particle_cut;
+using namespace corsika::units;
+using namespace corsika::units::si;
+
+TEST_CASE("ParticleCut", "[processes]") {
+  feenableexcept(FE_INVALID);
+  using EnvType = environment::Environment<setup::IEnvironmentModel>;
+  EnvType env;
+  const geometry::CoordinateSystem& rootCS = env.GetCoordinateSystem();
+  SECTION("cut") {
+
+    ParticleCut cut(20_GeV);
+
+    // setup particle stack, and add primary particle
+    setup::Stack stack;
+    stack.Clear();
+    const HEPEnergyType Eabove = 1_TeV;
+    const HEPEnergyType Ebelow = 10_GeV;
+    std::vector<particles::Code> particleList = {
+        particles::Code::PiPlus,   particles::Code::PiMinus, particles::Code::KPlus,
+        particles::Code::KMinus,   particles::Code::K0Long,  particles::Code::K0Short,
+        particles::Code::Electron, particles::Code::MuPlus,  particles::Code::NuE,
+        particles::Code::Neutron};
+    auto particle = stack.AddParticle(
+        std::tuple<particles::Code, units::si::HEPEnergyType,
+                   corsika::stack::MomentumVector, geometry::Point, units::si::TimeType>{
+	  particles::Code::Proton, Eabove,
+            corsika::stack::MomentumVector(rootCS, {0_GeV, 0_GeV, 0_GeV}),
+            geometry::Point(rootCS, 0_m, 0_m, 0_m), 0_ns});
+
+    for (auto proType : particleList)
+
+      particle.AddSecondary(std::tuple<particles::Code, units::si::HEPEnergyType,
+                                        corsika::stack::MomentumVector, geometry::Point,
+                                        units::si::TimeType>{
+          proType, Eabove, corsika::stack::MomentumVector(rootCS, {0_GeV, 0_GeV, 0_GeV}),
+          geometry::Point(rootCS, 0_m, 0_m, 0_m), 0_ns});
+
+    //    auto secViewPtr = corsika::stack::SecondaryView(particle);
+    corsika::stack::SecondaryView view(particle);
+    cut.DoSecondaries(view);
+    REQUIRE(stack.GetSize() == 1);
+    
+  }
+}
