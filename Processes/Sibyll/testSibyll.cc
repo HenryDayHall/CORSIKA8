85558652ea626091e82c507916339170fa909976
ralfulrich <ralf.ulrich@kit.edu>
2019-05-20 21:21:38 +0200

    added/updated copyright notice

--- Processes/Sibyll/testSibyll.cc
+++ Processes/Sibyll/testSibyll.cc
@@ -1,6 +1,6 @@
 
 /*
- * (c) Copyright 2018 CORSIKA Project, corsika-project@lists.kit.edu
+ * (c) Copyright 2019 CORSIKA Project, corsika-project@lists.kit.edu
  *
  * See file AUTHORS for a list of contributors.
  *
@@ -8,6 +8,7 @@
  * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
  * the license.
  */
+
 
 #include <corsika/process/sibyll/Decay.h>
 #include <corsika/process/sibyll/Interaction.h>
