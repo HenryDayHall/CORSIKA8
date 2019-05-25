b720e63a872f4d7bad3f8d75692a0b70c4a210e0
ralfulrich <ralf.ulrich@kit.edu>
2019-05-25 08:49:56 +0200

    added more options and better output

--- do-clang-format.sh
+++ do-clang-format.sh
@@ -4,7 +4,12 @@ command="clang-format -style=file `find . -iregex '^.*\.\(cc\|h\)$' -not -path '
 
 if [ "$1" == "check" ];
 then
-    ! ${command}  -output-replacements-xml | grep -c "<replacement " 
-else
+    `! ${command}  -output-replacements-xml | grep -qc "<replacement "` || \
+        { echo "format-check FAILED!"; exit $ERRCODE; }
+    echo "Congratulations: format-check succeeded"
+elif [ "$1" == "apply" ];
+then
     ${command} -i
+else
+    echo "please use: ./do-clang-format.sh [check] or [apply]"
 fi
