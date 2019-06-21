ffacd0e7f47ef336826dc0f2aa4703894b33c1c6
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2019-06-21 12:24:05 +0200

    removed asserts from cascade_example

--- Documentation/Examples/cascade_example.cc
+++ Documentation/Examples/cascade_example.cc
@@ -159,8 +159,4 @@ int main() {
        << "relative difference (%): " << (Efinal / E0 - 1) * 100 << endl;
   cout << "total dEdX energy (GeV): " << eLoss.GetTotal() / 1_GeV << endl
        << "relative difference (%): " << eLoss.GetTotal() / E0 * 100 << endl;
-
-  // basic check for unit-tests
-  assert(cut.GetNumberEmParticles() == 127);
-  assert(cut.GetNumberInvParticles() == 116);
 }
