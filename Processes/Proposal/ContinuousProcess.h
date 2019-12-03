2f99c859fedb716d5adc7c12943a17800d21f957
Maximilian Sackel <maximilian.sackel@gmx.de>
2019-12-03 09:39:34 +0100

    add continuous and interaction class-functions which has to be override

---
+++ Processes/Proposal/ContinuousProcess.h
@@ -0,0 +1,30 @@
+/*
+ * (c) Copyright 2018 CORSIKA Project, corsika-project@lists.kit.edu
+ *
+ * See file AUTHORS for a list of contributors.
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
+ * the license.
+ */
+
+#ifndef _corsika_process_proposal_interaction_h_
+#define _corsika_process_proposalythia_interaction_h_
+
+#include <PROPOSAL/PROPOSAL.h>
+
+
+namespace corsika::process::proposal {
+
+  class Interaction : public corsika::process::ContinuousProcess<Continuous> {
+  private:
+
+
+  public:
+    template <typename Particle, typename Track>
+    EProcessReturn DoContinuous(Particle&, Track const&) const;
+
+    template <typename Particle, typename Track>
+    units::si::LengthType MaxStepLength(Particle const& p, Track const& track) const;
+  }
+} // namespace corsika::process
