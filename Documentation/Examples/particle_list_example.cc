84de4c029dbe8f99406ad3125f453937c9e4cc31
Felix Riehn <felix@matilda>
2020-04-23 14:36:50 +0100

    add qgsjetII id

--- Documentation/Examples/particle_list_example.cc
+++ Documentation/Examples/particle_list_example.cc
@@ -10,6 +10,7 @@
 
 #include <corsika/particles/ParticleProperties.h>
 #include <corsika/process/ProcessSequence.h>
+#include <corsika/process/qgsjetII/ParticleConversion.h>
 #include <corsika/process/sibyll/ParticleConversion.h>
 #include <corsika/setup/SetupEnvironment.h>
 #include <corsika/units/PhysicalUnits.h>
@@ -25,23 +26,26 @@ using namespace std;
 // The example main program for a particle list
 //
 int main() {
-  cout << "--------------------"
+  cout << "------------------------------------------"
        << "particles in CORSIKA"
-       << "--------------------" << endl;
+       << "------------------------------------------" << endl;
   cout << std::setw(20) << "Name"
        << " | " << std::setw(10) << "PDG-id"
        << " | " << std::setw(10) << "SIBYLL-id"
+       << " | " << std::setw(10) << "QGSJETII-id"
        << " | " << std::setw(18) << "PDG-mass (GeV)"
        << " | " << std::setw(18) << "SIBYLL-mass (GeV)"
        << " | " << endl;
-  cout << std::setw(58) << std::setfill('-') << "-" << endl;
+  cout << std::setw(104) << std::setfill('-') << "-" << endl;
   for (auto p : corsika::particles::detail::all_particles) {
     if (p != Code::Unknown && !IsNucleus(p))
       cout << std::setw(20) << std::setfill(' ') << p << " | " << std::setw(10)
            << static_cast<int>(GetPDG(p)) << " | " << std::setw(10)
            << static_cast<int>(corsika::process::sibyll::ConvertToSibyll(p)) << " | "
+           << std::setw(10)
+           << static_cast<int>(corsika::process::qgsjetII::ConvertToQgsjetII(p)) << " | "
            << std::setw(18) << GetMass(p) / 1_GeV << " | " << std::setw(18)
            << corsika::process::sibyll::GetSibyllMass(p) / 1_GeV << " | " << endl;
   }
-  cout << std::setw(54) << std::setfill('-') << "-" << endl;
+  cout << std::setw(104) << std::setfill('-') << "-" << endl;
 }
