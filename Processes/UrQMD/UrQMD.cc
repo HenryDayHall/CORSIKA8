4ff862c64a9d5af811921fd24de44c81ef51d544
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2020-06-08 19:53:09 +0200

    Revert "UrQMD impact parameter range from C7 for O,N,Ar"
    This reverts commit 690cfe3eb14efc7e90022872e831b8b5d6c94e2a.

--- Processes/UrQMD/UrQMD.cc
+++ Processes/UrQMD/UrQMD.cc
@@ -293,19 +293,8 @@ corsika::process::EProcessReturn UrQMD::DoInteraction(SetupProjectile& vProjecti
   } else {
     inputs_.prspflg = 1;
     sys_.Ap = 1; // even for non-baryons this has to be set, see vanilla UrQMD.f
+    rsys_.bdist = nucrad_(targetA) + nucrad_(1) + 2 * options_.CTParam[30 - 1];
     rsys_.ebeam = (projectileEnergyLab - vProjectile.GetMass()) * (1 / 1_GeV);
-
-    switch (targetCode) {
-      case particles::Code::Nitrogen:
-      case particles::Code::Oxygen:
-        rsys_.bdist = 6;
-        break;
-      case particles::Code::Argon:
-        rsys_.bdist = 7;
-        break;
-      default:
-        rsys_.bdist = nucrad_(targetA) + nucrad_(1) + 2 * options_.CTParam[30 - 1];
-    }
 
     if (projectileCode == particles::Code::K0Long ||
         projectileCode == particles::Code::K0Short) {
