7ca2ee5dd64865eff1caf77ef3d054942d05739b
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2020-06-12 17:14:06 +0200

    use ShowerAxis in stoppingPower.cc

--- Documentation/Examples/stopping_power.cc
+++ Documentation/Examples/stopping_power.cc
@@ -9,6 +9,7 @@
  */
 
 #include <corsika/environment/Environment.h>
+#include <corsika/environment/ShowerAxis.h>
 #include <corsika/geometry/Sphere.h>
 #include <corsika/process/energy_loss/EnergyLoss.h>
 #include <corsika/setup/SetupStack.h>
@@ -44,9 +45,9 @@ int main() {
       rootCS, 0_m, 0_m,
       112.8_km); // this is the CORSIKA 7 start of atmosphere/universe
 
-  Vector<dimensionless_d> showerAxis(rootCS, {0, 0, -1});
-
-  process::energy_loss::EnergyLoss eLoss(injectionPos, showerAxis);
+  environment::ShowerAxis showerAxis{injectionPos,
+                                     Vector<length_d>{rootCS, 0_m, 0_m, 1_m}, env};
+  process::energy_loss::EnergyLoss eLoss{showerAxis};
 
   setup::Stack stack;
 
