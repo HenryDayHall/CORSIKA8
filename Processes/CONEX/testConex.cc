184750f6fe1171b8d9c4260b97d096bd6862ade3
ralfulrich <ralf.ulrich@kit.edu>
2020-07-03 01:03:30 +0200

    fixed Init call

--- Processes/CONEX/testConex.cc
+++ Processes/CONEX/testConex.cc
@@ -38,7 +38,7 @@ TEST_CASE("CONEX", "[processes]") {
     int nShower = 1; // large to avoid final stats.
     int maxDetail = 0;
     int particleListMode = 0;
-    cx.Init(nShower, randomSeeds, cxModel, maxDetail, particleListMode,
+    cx.Init(nShower, randomSeeds, maxDetail, particleListMode,
             parameterPathName);
 
     double energyInGeV = 100.;
