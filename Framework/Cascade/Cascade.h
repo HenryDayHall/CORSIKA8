12790a84d5d2ac6add053b7898ecfb241416d2c8
André Schmidt <upwli@student.kit.edu>
2020-07-16 12:21:42 +0200

    Update Cascade.h

--- Framework/Cascade/Cascade.h
+++ Framework/Cascade/Cascade.h
@@ -227,7 +227,7 @@ namespace corsika::cascade {
       std::cout << "distance_max=" << distance_max << std::endl;
 
       // take minimum of geometry, interaction, decay for next step
-      auto const min_distance =std::min(
+      auto const min_distance = std::min(
           {distance_interact, distance_decay, distance_max, geomMaxLength, magMaxLength});
 
       C8LOG_DEBUG("transport particle by : {} m", min_distance / 1_m);
