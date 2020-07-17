e1186f88ce3e5707ab633d043f1b985485ff1ebc
ralfulrich <ralf.ulrich@kit.edu>
2020-07-17 13:38:13 +0200

    remove double-line from copyright message

--- Framework/Testing/TestMain.cc
+++ Framework/Testing/TestMain.cc
@@ -1,7 +1,6 @@
 /*
  * (c) Copyright 2019 CORSIKA Project, corsika-project@lists.kit.edu
  *
- *
  * This software is distributed under the terms of the GNU General Public
  * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
  * the license.
