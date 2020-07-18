e2c00791cbb60d9b88a39def0863697ebded46a6
Remy Prechelt <prechelt@hawaii.edu>
2020-07-17 22:26:12 -1000

    Add new logging implementation using spdlog.

---
+++ Framework/Logging/Logging.h
@@ -0,0 +1,96 @@
+/*
+ * (c) Copyright 2020 CORSIKA Project, corsika-project@lists.kit.edu
+ *
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
+ * the license.
+ */
+
+/**
+   @File Logging.h
+
+   CORSIKA8 logging utilities.
+ */
+
+#pragma once
+
+#include <spdlog/spdlog.h>
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+
+namespace corsika::logging {
+
+  // bring spdlog into the corsika::logging namespace
+  using namespace spdlog;
+
+  /**
+   * Create a new C8-style logger.
+   *
+   * @param name           The unique name of the logger.
+   * @param defaultlog     If True, set this as the default logger.
+   * @returns              The constructed and formatted logger.
+   */
+  auto CreateLogger(std::string const& name, bool const defaultlog = false) {
+
+    // create the logger
+    auto logger = spdlog::stdout_color_mt(name);
+
+    // set the default C8 format
+    logger->set_pattern("[%n:%^%-8l%$] %v");
+
+    // if defaultlog is True, we set this as the default spdlog logger.
+    if (defaultlog) { spdlog::set_default_logger(logger); }
+
+    // and return the logger
+    return logger;
+  }
+
+  /**
+   * Get a reference to a named logger.
+   *
+   * If the logger does not exist, it is created.
+   *
+   * @param name    The name of the logger to get.
+   */
+  auto GetLogger(std::string const& name, bool const defaultlog = false) {
+
+    // attempt to get the logger from the registry
+    auto logger = spdlog::get(name);
+
+    // weg found the logger, so just return it
+    if (logger) {
+      return logger;
+    } else { // logger was not found so create it
+      return CreateLogger(name, defaultlog);
+    }
+  }
+
+  /**
+   * Set the default log level for all loggers.
+   *
+   * @param name    The minimum log level required to print.
+   *
+   */
+  auto SetDefaultLevel(level::level_enum minlevel) -> void {
+    spdlog::set_level(minlevel);
+  }
+
+  // create the default CORSIKA logger
+  inline auto corsika = GetLogger("corsika", true);
+
+// define our macro-style loggers
+#define C8LOG_DEBUG SPDLOG_DEBUG
+#define C8LOG_INFO SPDLOG_INFO
+#define C8LOG_WARN SPDLOG_WARN
+#define C8LOG_ERROR SPDLOG_ERROR
+#define C8LOG_CRITICAL SPDLOG_CRITICAL
+
+// and the specific logger versions
+#define C8LOG_LOGGER_DEBUG SPDLOG_LOGGER_DEBUG
+#define C8LOG_LOGGER_INFO SPDLOG_LOGGER_INFO
+#define C8LOG_LOGGER_WARN SPDLOG_LOGGER_WARN
+#define C8LOG_LOGGER_ERROR SPDLOG_LOGGER_ERROR
+#define C8LOG_LOGGER_CRITICAL SPDLOG_LOGGER_CRITICAL
+
+} // namespace corsika::logging
--- Framework/Logging/testLogging.cc
+++ Framework/Logging/testLogging.cc
@@ -1,23 +1,94 @@
 /*
- * (c) Copyright 2018 CORSIKA Project, corsika-project@lists.kit.edu
+ * (c) Copyright 2020 CORSIKA Project, corsika-project@lists.kit.edu
  *
  * This software is distributed under the terms of the GNU General Public
  * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
  * the license.
  */
 
-#include <corsika/logging/Logger.h>
+#include <corsika/logging/Logging.h>
 
 #include <catch2/catch.hpp>
 
-/*
-  this does nothing yet
- */
+using namespace corsika;
 
 TEST_CASE("Logging", "[Logging]") {
-  SECTION("sectionOne") {}
+  SECTION("top level functions using corsika logger") {
+    logging::info("This is an info message!");
+    logging::warn("This is a warning message!");
+    logging::debug("This is a debug message!");
+    logging::error("This is an error message!");
+    logging::critical("This is a critical error message!");
+  }
 
-  SECTION("sectionTwo") {}
+  SECTION("create a specific logger") {
 
-  SECTION("sectionThree") {}
+    // create a logger manually
+    auto logger = logging::CreateLogger("loggerA");
+
+    // set a custom pattern for this logger
+    logger->set_pattern("[%n:%^%-8l%$] custom pattern: %v");
+
+    // and make sure we can log with this created object
+    logger->info("This is an info message!");
+    logger->warn("This is a warning message!");
+    logger->debug("This is a debug message!");
+    logger->error("This is an error message!");
+    logger->critical("This is a critical error message!");
+
+    // get a reference to the logger using Get
+    auto other = logging::GetLogger("loggerA");
+
+    // and make sure we can use this other reference to log
+    other->info("This is an info message!");
+    other->warn("This is a warning message!");
+    other->debug("This is a debug message!");
+    other->error("This is an error message!");
+    other->critical("This is a critical error message!");
+  }
+
+  SECTION("get a new logger") {
+
+    // get a reference to an unknown logger
+    auto logger = logging::GetLogger("loggerB");
+
+    // and make sure we can log with this created object
+    logger->info("This is an info message!");
+    logger->warn("This is a warning message!");
+    logger->debug("This is a debug message!");
+    logger->error("This is an error message!");
+    logger->critical("This is a critical error message!");
+  }
+
+  SECTION("test log level") {
+
+    // set the default log level
+    logging::SetDefaultLevel(logging::level::critical);
+
+    // and make sure we can log with this created object
+    logging::info("This should NOT be printed!");
+    logging::warn("This should NOT be printed!");
+    logging::debug("This should NOT be printed!");
+    logging::error("This should NOT be printed!");
+    logging::critical("This SHOULD BE printed!!");
+
+    // and reset it for the next tests
+    logging::SetDefaultLevel(logging::level::debug);
+  }
+
+  SECTION("test macro style printing") {
+
+    // these print with the "corsika" logger
+    C8LOG_INFO("This is a macro info msg!");
+    C8LOG_DEBUG("This is a macro debug msg!");
+    C8LOG_ERROR("This is a macro error msg!");
+    C8LOG_CRITICAL("This is a macro critical msg!");
+
+    // get a reference to an unknown logger
+    auto logger = logging::GetLogger("loggerD");
+
+    // these print with the "loggerD" logger
+    C8LOG_LOGGER_INFO(logger, "This is a macro info msg!");
+    C8LOG_LOGGER_WARN(logger, "This is a macro warn msg!");
+  }
 }
