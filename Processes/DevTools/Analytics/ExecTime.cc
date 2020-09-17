1bbdb0d223c0a48b3df1ca32e3287772a383afdb
Dominik Baack <dominik.baack@tu-dortmund.de>
2020-09-17 17:09:14 +0200

    Added more functionality to ExecTime

--- Processes/DevTools/Analytics/ExecTime.cc
+++ Processes/DevTools/Analytics/ExecTime.cc
@@ -15,10 +15,50 @@ namespace corsika::process {
   namespace devtools {
 
     template <class T>
-    void ExecTime<T>::start() {}
+    void ExecTime<T>::start() {
+      fStart = std::chrono::steady_clock::now();
+    }
 
     template <class T>
-    void ExecTime<T>::stop() {}
+    void ExecTime<T>::stop() {
+      auto end = std::chrono::steady_clock::now();
+      std::chrono::microseconds timeDiv =
+          std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+      fElapsedSum += timeDiv;
+      fN++;
+
+      if (fMax < timeDiv) fMax = timeDiv;
+
+      if (timeDiv < fMin) fMin = timeDiv;
+
+      auto delta = timeDiv - fMean;
+      fMean += delta / fN;
+
+      auto delta2 = timeDiv - fMean;
+
+      fMean2 += delta * delta2;
+    }
+
+    template <typename T>
+    double ExecTime<T>::mean() {
+      return fMean;
+    }
+
+    template <typename T>
+    double ExecTime<T>::var() {
+      return fMean2 / count;
+    }
+
+    template <typename T>
+    double ExecTime<T>::min() {
+      return fMin;
+    }
+
+    template <typename T>
+    double ExecTime<T>::max() {
+      return fMax;
+    }
 
   } // namespace devtools
 } // namespace corsika::process
