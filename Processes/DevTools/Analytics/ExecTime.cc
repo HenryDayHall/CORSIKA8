668ee2cfb039f39e6d6016334968e3d62acb2e41
Dominik Baack <dominik.baack@tu-dortmund.de>
2020-09-18 14:22:30 +0200

    fixed template class stuff added missing unit test

--- Processes/DevTools/Analytics/ExecTime.cc
+++ Processes/DevTools/Analytics/ExecTime.cc
@@ -14,51 +14,90 @@
 namespace corsika::process {
   namespace devtools {
 
-    template <class T>
-    void ExecTime<T>::start() {
-      fStart = std::chrono::steady_clock::now();
-    }
-
-    template <class T>
-    void ExecTime<T>::stop() {
-      auto end = std::chrono::steady_clock::now();
-      std::chrono::microseconds timeDiv =
-          std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-      fElapsedSum += timeDiv;
-      fN++;
-
-      if (fMax < timeDiv) fMax = timeDiv;
-
-      if (timeDiv < fMin) fMin = timeDiv;
-
-      auto delta = timeDiv - fMean;
-      fMean += delta / fN;
-
-      auto delta2 = timeDiv - fMean;
-
-      fMean2 += delta * delta2;
-    }
-
-    template <typename T>
-    double ExecTime<T>::mean() {
-      return fMean;
-    }
-
-    template <typename T>
-    double ExecTime<T>::var() {
-      return fMean2 / count;
-    }
-
-    template <typename T>
-    double ExecTime<T>::min() {
-      return fMin;
-    }
-
-    template <typename T>
-    double ExecTime<T>::max() {
-      return fMax;
-    }
+ 
 
   } // namespace devtools
 } // namespace corsika::process
+
+/*
+ template <typename T, bool>
+    class ExecTime_BoundaryCrossing {};
+
+    template <typename T>
+    class ExecTime_BoundaryCrossing<T, true> : protected T {
+    public:
+      template <
+          typename Particle, typename VTNType,
+          typename std::enable_if_t<
+              std::is_base_of<BoundaryCrossingProcess<typename T::_TDerived>, T>::value,
+              int> = 0>
+      EProcessReturn DoBoundaryCrossing(Particle& p, VTNType const& from,
+                                        VTNType const& to) {
+
+        return T::DoBoundaryCrossing(p, from, to);
+      }
+    };
+
+    template <typename T, bool>
+    class ExecTime_Continuous {};
+
+    template <typename T>
+    class ExecTime_Continuous<T, true> : protected T {
+    public:
+      template <typename Particle, typename Track>
+      EProcessReturn DoContinuous(Particle& p, Track const& t) const {
+        return T::DoContinous(p, t);
+      }
+
+      template <typename Particle, typename Track>
+      units::si::LengthType MaxStepLength(Particle const& p, Track const& track) const {
+        return T::MaxStepLength(p, track);
+      }
+    };
+
+    template <typename T, bool>
+    class ExecTime_Decay {};
+
+    template <typename T>
+    class ExecTime_Decay<T, true> : protected T {
+    public:
+      template <typename Particle>
+      EProcessReturn DoDecay(Particle& p) {
+        return T::DoDecay(p);
+      }
+
+      template <typename Particle>
+      corsika::units::si::TimeType GetLifetime(Particle& p) {
+        return T::GetLifetime(p);
+      }
+    };
+
+    template <typename T, bool>
+    class ExecTime_Interaction {};
+
+    template <typename T>
+    class ExecTime_Interaction<T, true> : protected T {
+    public:
+      template <typename Particle>
+      EProcessReturn DoInteraction(Particle& p) {
+        return T::DoInteraction(p);
+      }
+
+      template <typename Particle>
+      corsika::units::si::GrammageType GetInteractionLength(Particle& p) {
+        return T::GetInteractionLength(p);
+      }
+    };
+
+    template <typename T, bool>
+    class ExecTime_Secondaries {};
+
+    template <typename T>
+    class ExecTime_Secondaries<T, true> : protected T {
+    public:
+      template <typename Secondaries>
+      inline EProcessReturn DoSecondaries(Secondaries& sec) {
+        return T::DoSecondaries(sec);
+      }
+    };
+    */
