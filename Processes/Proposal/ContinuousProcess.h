bfc401d5754b6750ec272c7614a95c72fa438468
Maximilian Sackel <maximilian.sackel@tu-dortmund.de>
2020-09-29 15:23:28 +0000

    start documenting classes and summaries duplicate code in one abstract class

--- Processes/Proposal/ContinuousProcess.h
+++ Processes/Proposal/ContinuousProcess.h
@@ -13,102 +13,70 @@
 #include <corsika/particles/ParticleProperties.h>
 #include <corsika/process/ContinuousProcess.h>
 #include <corsika/process/particle_cut/ParticleCut.h>
+#include <corsika/process/proposal/ProposalProcessBase.h>
 #include <corsika/random/RNGManager.h>
 #include <corsika/random/UniformRealDistribution.h>
 #include <unordered_map>
 
-using std::unordered_map;
-
-using namespace corsika::environment;
-using namespace corsika::units::si;
-
-using CORSIKA_ParticleCut = corsika::process::particle_cut::ParticleCut;
-
 namespace corsika::process::proposal {
 
-  class ContinuousProcess
-      : public corsika::process::ContinuousProcess<ContinuousProcess> {
-    CORSIKA_ParticleCut& cut;
-    corsika::random::RNG& fRNG;
-    static constexpr std::array<particles::Code, 7> tracked_particles{
-        particles::Code::Gamma,    particles::Code::Electron, particles::Code::Positron,
-        particles::Code::MuMinus,  particles::Code::MuPlus,   particles::Code::TauPlus,
-        particles::Code::TauMinus,
-    };
-    unordered_map<const NuclearComposition*, PROPOSAL::Medium> media;
+  using namespace corsika::units::si;
 
-    bool CanInteract(particles::Code pcode) const noexcept;
-
-    using calc_key_t = std::pair<const NuclearComposition*, particles::Code>;
-    using calc_t =
-        tuple<unique_ptr<PROPOSAL::Displacement>, unique_ptr<PROPOSAL::Scattering>>;
-
-    struct disp_hash {
-      size_t operator()(const calc_key_t& p) const {
-        return std::hash<const NuclearComposition*>{}(p.first) ^
-               std::hash<particles::Code>{}(p.second);
-      }
-    };
+  //!
+  //! Electro-magnetic and gamma continous losses produced by proposal. It makes
+  //! use of interpolation tables which are runtime intensive calculation, but can be
+  //! reused by setting the \param PROPOSAL::InterpolationDef::path_to_tables variable.
+  //!
+  class ContinuousProcess : public process::ContinuousProcess<ContinuousProcess>,
+                            ProposalProcessBase {
 
     enum { DISPLACEMENT, SCATTERING };
-    unordered_map<calc_key_t, calc_t, disp_hash> calc;
+    using calc_t = std::tuple<std::unique_ptr<PROPOSAL::Displacement>,
+                              std::unique_ptr<PROPOSAL::Scattering>>;
 
-    template <typename Particle>
-    auto BuildCalculator(particles::Code code, Particle p_def,
-                         NuclearComposition const& comp) {
-      auto cross = GetStdCrossSections(
-          p_def, media.at(&comp),
-          make_shared<const PROPOSAL::EnergyCutSettings>(cut.GetECut() / 1_MeV, 1, false),
-          true);
-      auto [insert_it, success] =
-          calc.insert({std::make_pair(&comp, code),
-                       std::make_tuple(PROPOSAL::make_displacement(cross, true),
-                                       PROPOSAL::make_scattering("highland", p_def,
-                                                                 media.at(&comp)))});
-      return insert_it;
-    }
+    std::unordered_map<calc_key_t, calc_t, hash>
+        calc; //!< Stores the displacement and scattering calculators.
 
-    auto BuildCalculator(particles::Code corsika_code, NuclearComposition const& comp) {
-      if (corsika_code == particles::Code::Gamma)
-        return BuildCalculator(particles::Code::Gamma, PROPOSAL::GammaDef(), comp);
-      if (corsika_code == particles::Code::Electron)
-        return BuildCalculator(particles::Code::Electron, PROPOSAL::EMinusDef(), comp);
-      if (corsika_code == particles::Code::Positron)
-        return BuildCalculator(particles::Code::Positron, PROPOSAL::EPlusDef(), comp);
-      if (corsika_code == particles::Code::MuMinus)
-        return BuildCalculator(particles::Code::MuMinus, PROPOSAL::MuMinusDef(), comp);
-      if (corsika_code == particles::Code::MuPlus)
-        return BuildCalculator(particles::Code::MuPlus, PROPOSAL::MuPlusDef(), comp);
-      if (corsika_code == particles::Code::TauMinus)
-        return BuildCalculator(particles::Code::TauMinus, PROPOSAL::TauMinusDef(), comp);
-      if (corsika_code == particles::Code::TauPlus)
-        return BuildCalculator(particles::Code::TauPlus, PROPOSAL::TauPlusDef(), comp);
-      throw std::runtime_error("PROPOSAL could not find corresponding builder");
-    }
-
-    template <typename Particle>
-    auto GetCalculator(Particle& vP) {
-      auto& comp = vP.GetNode()->GetModelProperties().GetNuclearComposition();
-      auto calc_it = calc.find(std::make_pair(&comp, vP.GetPID()));
-      if (calc_it != calc.end()) return calc_it;
-      return BuildCalculator(vP.GetPID(), comp);
-    }
+    //!
+    //! Build the displacement and scattering calculators and add it to calc.
+    //!
+    void BuildCalculator(particles::Code, environment::NuclearComposition const&) final;
 
   public:
+    //! Produces the continuous loss calculator for leptons based on nuclear
+    //! compositions and stochastic description limited by the particle cut.
+    //!
     template <typename TEnvironment>
-    ContinuousProcess(TEnvironment const&, CORSIKA_ParticleCut&);
+    ContinuousProcess(TEnvironment const&, particle_cut::ParticleCut&);
 
+    //!
+    //! Calculates the energy of the particle which has passed the given
+    //! grammage.
+    //!
     template <typename Particle>
     corsika::units::si::HEPEnergyType TotalEnergyLoss(
         Particle const&, corsika::units::si::GrammageType const&);
 
+    //!
+    //! Multiple Scattering of the lepton. Stochastic deflection is not yet taken into
+    //! account. Displacment of the track due to multiple scattering is not possible
+    //! because of the constant referernce. The final direction will be updated anyway.
+    //!
     template <typename Particle>
     void Scatter(Particle&, corsika::units::si::HEPEnergyType const&,
                  corsika::units::si::GrammageType const&);
 
+    //!
+    //! Produces the loss and deflection after given distance for the particle.
+    //! If the particle if below the given energy threshold where it will be
+    //! considered stochastically, it will be absorbed.
+    //!
     template <typename Particle, typename Track>
     EProcessReturn DoContinuous(Particle&, Track const&);
 
+    //!
+    //! Calculates maximal step length of process.
+    //!
     template <typename Particle, typename Track>
     corsika::units::si::LengthType MaxStepLength(Particle const&, Track const&);
   };
--- Processes/Proposal/Interaction.cc
+++ Processes/Proposal/Interaction.cc
@@ -8,6 +8,7 @@
 
 #include <corsika/environment/IMediumModel.h>
 #include <corsika/environment/NuclearComposition.h>
+#include <corsika/process/particle_cut/ParticleCut.h>
 #include <corsika/process/proposal/Interaction.h>
 #include <corsika/setup/SetupEnvironment.h>
 #include <corsika/setup/SetupStack.h>
@@ -19,55 +20,34 @@
 #include <random>
 #include <tuple>
 
-using Component_PROPOSAL = PROPOSAL::Components::Component;
-
 namespace corsika::process::proposal {
-  using namespace corsika::setup;
   using namespace corsika::environment;
   using namespace corsika::units::si;
 
-  bool Interaction::CanInteract(particles::Code pcode) const noexcept {
-    if (std::find(tracked_particles.begin(), tracked_particles.end(), pcode) !=
-        tracked_particles.end())
-      return true;
-    return false;
-  }
-
   template <>
-  Interaction::Interaction(SetupEnvironment const& _env, CORSIKA_ParticleCut& _cut)
-      : cut(_cut)
-      , fRNG(corsika::random::RNGManager::GetInstance().GetRandomStream("proposal")) {
-    auto all_compositions = std::vector<const NuclearComposition*>();
-    _env.GetUniverse()->walk([&](auto& vtn) {
-      if (vtn.HasModelProperties())
-        all_compositions.push_back(&vtn.GetModelProperties().GetNuclearComposition());
-    });
-    for (auto& ncarg : all_compositions) {
-      auto comp_vec = std::vector<Component_PROPOSAL>();
-      auto frac_iter = ncarg->GetFractions().cbegin();
-      for (auto& pcode : ncarg->GetComponents()) {
-        comp_vec.emplace_back(GetName(pcode), GetNucleusZ(pcode), GetNucleusA(pcode),
-                              *frac_iter);
-        ++frac_iter;
-      }
-      media[ncarg] = PROPOSAL::Medium(
-          "Modified Air", PROPOSAL::Air().GetI(), PROPOSAL::Air().GetC(),
-          PROPOSAL::Air().GetA(), PROPOSAL::Air().GetM(), PROPOSAL::Air().GetX0(),
-          PROPOSAL::Air().GetX1(), PROPOSAL::Air().GetD0(), 1.0, comp_vec);
-    }
+  Interaction::Interaction(setup::SetupEnvironment const& _env,
+                           particle_cut::ParticleCut& _cut)
+      : ProposalProcessBase(_env, _cut) {}
+
+  void Interaction::BuildCalculator(particles::Code code,
+                                    environment::NuclearComposition const& comp) {
+    auto c = cross[code](media.at(&comp), cut);
+    auto inter_types = PROPOSAL::CrossSectionVector::GetInteractionTypes(c);
+    calc[std::make_pair(&comp, code)] = std::make_tuple(
+        PROPOSAL::make_secondaries(inter_types, particle[code], media.at(&comp)),
+        PROPOSAL::make_interaction(c, true));
   }
 
   template <>
   corsika::process::EProcessReturn Interaction::DoInteraction(
       setup::StackView::StackIterator& vP) {
     if (CanInteract(vP.GetPID())) {
-      auto calc = GetCalculator(vP); // [CrossSections]
+      auto c = GetCalculator(vP, calc); // [CrossSections]
       std::uniform_real_distribution<double> distr(0., 1.);
-      auto [type, comp_ptr, v] =
-          get<INTERACTION>(calc->second)
-              ->TypeInteraction(vP.GetEnergy() / 1_MeV, distr(fRNG));
-      auto rnd =
-          vector<double>(get<SECONDARIES>(calc->second)->RequiredRandomNumbers(type));
+      auto rates = get<INTERACTION>(c->second)->Rates(vP.GetEnergy() / 1_MeV);
+      auto [type, comp_ptr, v] = get<INTERACTION>(c->second)->SampleLoss(
+          vP.GetEnergy() / 1_MeV, rates, distr(fRNG));
+      auto rnd = vector<double>(get<SECONDARIES>(c->second)->RequiredRandomNumbers(type));
       for (auto& it : rnd) it = distr(fRNG);
       auto point = PROPOSAL::Vector3D(vP.GetPosition().GetX() / 1_cm,
                                       vP.GetPosition().GetY() / 1_cm,
@@ -77,8 +57,8 @@ namespace corsika::process::proposal {
                                           d.GetZ().magnitude());
       auto loss = make_tuple(static_cast<int>(type), point, direction,
                              v * vP.GetEnergy() / 1_MeV, 0.);
-      auto sec = get<SECONDARIES>(calc->second)
-                     ->CalculateSecondaries(vP.GetEnergy() / 1_MeV, loss, *comp_ptr, rnd);
+      auto sec = get<SECONDARIES>(c->second)->CalculateSecondaries(vP.GetEnergy() / 1_MeV,
+                                                                   loss, *comp_ptr, rnd);
       for (auto& s : sec) {
         auto E = get<PROPOSAL::Loss::ENERGY>(s) * 1_MeV;
         auto vec = corsika::geometry::QuantityVector(
@@ -101,8 +81,8 @@ namespace corsika::process::proposal {
   corsika::units::si::GrammageType Interaction::GetInteractionLength(
       setup::Stack::StackIterator const& vP) {
     if (CanInteract(vP.GetPID())) {
-      auto calc = GetCalculator(vP);
-      return get<INTERACTION>(calc->second)->MeanFreePath(vP.GetEnergy() / 1_MeV) * 1_g /
+      auto c = GetCalculator(vP, calc);
+      return get<INTERACTION>(c->second)->MeanFreePath(vP.GetEnergy() / 1_MeV) * 1_g /
              (1_cm * 1_cm);
     }
     return std::numeric_limits<double>::infinity() * 1_g / (1_cm * 1_cm);
