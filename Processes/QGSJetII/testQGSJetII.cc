55eb25992446ed613b01bc957a1fe192b70218b6
ralfulrich <ralf.ulrich@kit.edu>
2020-10-02 00:45:29 +0200

    format

--- Processes/QGSJetII/testQGSJetII.cc
+++ Processes/QGSJetII/testQGSJetII.cc
@@ -145,12 +145,12 @@ TEST_CASE("QgsjetIIInterface", "[processes]") {
     CHECK(length / (1_g / square(1_cm)) == Approx(93.47).margin(0.1));
 
     /***********************************
-     It as turned out already two times (#291 and #307) that the detailed output of QGSJetII
-     event generation depends on the gfortran version used. This is not reliable and cannot
-     be tested in a unit test here. 
-     One related problem was already found (#291) and is realted to undefined behaviour in the
-     evaluation of functions in logical expressions. It is not clear if #307 is the same issue.      
-    
+     It as turned out already two times (#291 and #307) that the detailed output of
+    QGSJetII event generation depends on the gfortran version used. This is not reliable
+    and cannot be tested in a unit test here. One related problem was already found (#291)
+    and is realted to undefined behaviour in the evaluation of functions in logical
+    expressions. It is not clear if #307 is the same issue.
+
      CHECK(view.GetSize() == 14);
      CHECK(sumCharge(view) == 2);
     ************************************/
