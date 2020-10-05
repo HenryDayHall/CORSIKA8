953e071da9f19988edba3690d0f35a2a07f566c1
Dominik Baack <dominik.baack@tu-dortmund.de>
2020-10-05 14:56:23 +0200

    New timer class added for cleaner structure

---
+++ Framework/Analytics/testAnalytics.cc
@@ -0,0 +1,61 @@
+/*
+ * (c) Copyright 2020 CORSIKA Project, corsika-project@lists.kit.edu
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
+ * the license.
+ */
+
+#include <corsika/analytics/Timer.h>
+
+#include <catch2/catch.hpp>
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+using namespace corsika;
+
+int testFunc() {
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  return 31415;
+}
+
+class testClass {
+public:
+  int testFunc() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    return 31415;
+  }
+
+  int operator()() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    return 31415;
+  }
+};
+
+TEST_CASE("Analytics", "[Timer]") {
+  SECTION("Measure runtime of a free function") {
+
+    auto test = corsika::analytics::timeFunction(testFunc);
+
+    std::cout << test() << std::endl;
+    std::cout << test.getTime().count() << std::endl;
+  }
+
+  SECTION("Measure runtime of a class functor") {
+    testClass testC;
+    auto test = corsika::analytics::timeFunction(testC);
+
+    std::cout << test() << std::endl;
+    std::cout << test.getTime().count() << std::endl;
+  }
+
+  SECTION("Measure runtime of a class member function") {
+    testClass testC;
+    auto test = corsika::analytics::timeProxy<testClass>();
+
+    std::cout << test() << std::endl;
+    std::cout << test.getTime().count() << std::endl;
+  }
+}
