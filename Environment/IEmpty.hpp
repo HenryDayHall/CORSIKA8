c88e0e6d12f0b2f78baf8b19bf4fb70906192918
rulrich <ralf.m.ulrich@kit.edu>
2020-11-13 12:22:59 +0100

    virtual destructor

--- Environment/IEmpty.hpp
+++ Environment/IEmpty.hpp
@@ -30,6 +30,8 @@ namespace corsika::environment {
     virtual corsika::units::si::LengthType ArclengthFromGrammage(
         corsika::geometry::LineTrajectory const&,
         corsika::units::si::GrammageType) const = 0;
+    
+    virtual ~IEmpty() {}
   };
 
   template <typename TModel = IEmpty>
