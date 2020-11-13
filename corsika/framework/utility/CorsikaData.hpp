f2715107ce09b5561aac8083d284ff4119b7e02d
Dominik Baack <dominik.baack@tu-dortmund.de>
2020-11-13 22:32:04 +0100

    Added CorsikaData

---
+++ corsika/framework/utility/CorsikaData.hpp
@@ -0,0 +1,20 @@
+/*
+ * (c) Copyright 2020 CORSIKA Project, corsika-project@lists.kit.edu
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
+ * the license.
+ */
+
+#pragma once
+
+#include <string>
+
+namespace corsika {
+  /**
+   * returns the full path of the file \p filename within the CORSIKA_DATA directory
+   */
+  std::string CorsikaData(std::string const& filename);
+} // namespace corsika
+
+#include <corsika/detail/framework/utility/CorsikaData.inl>
