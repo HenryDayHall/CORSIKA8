06b1c98792f77172f797b30453195e78123fa53b
Felix Riehn <felix@matilda>
2021-01-28 12:11:21 +0000

    clang again

--- corsika/modules/ParticleCut.hpp
+++ corsika/modules/ParticleCut.hpp
@@ -58,7 +58,7 @@ namespace corsika {
     }
 
     void printThresholds();
-    void showResults(); // LCOV_EXCL_LINE 
+    void showResults(); // LCOV_EXCL_LINE
     void reset();
 
     HEPEnergyType getElectronECut() const { return get_energy_threshold(Code::Electron); }
