ad1fdd9312979f30f32697b96bfb3f5c2d01a1c8
ralfulrich <ralf.ulrich@kit.edu>
2021-03-04 20:28:23 +0100

    updated min number of argc

--- examples/vertical_EAS.cpp
+++ examples/vertical_EAS.cpp
@@ -103,7 +103,7 @@ int main(int argc, char** argv) {
 
   CORSIKA_LOG_INFO("vertical_EAS");
 
-  if (argc < 4) {
+  if (argc < 5) {
     std::cerr << "usage: vertical_EAS <A> <Z> <energy/GeV> [seed] \n"
                  "       if A=0, Z is interpreted as PDG code \n"
                  "       if no seed is given, a random seed is chosen \n"
