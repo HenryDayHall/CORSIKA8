c34cdb3211f75b1759710a4095ba0d2e76037537
ralfulrich <ralf.ulrich@kit.edu>
2021-06-06 11:15:34 +0200

    copyright notices

--- corsika/detail/framework/random/random_iterator/detail/Squares3_128.hpp
+++ corsika/detail/framework/random/random_iterator/detail/Squares3_128.hpp
@@ -1,23 +1,11 @@
-/*----------------------------------------------------------------------------
+/*
+ * (c) Copyright 2021 CORSIKA Project, corsika-project@lists.kit.edu
  *
- *   Copyright (C) 2021 Antonio Augusto Alves Junior
- *
- *   This file is part of RandomIterator.
- *
- *   RandomIterator is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   RandomIterator is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with RandomIterator.  If not, see <http://www.gnu.org/licenses/>.
- *
- *---------------------------------------------------------------------------*/
+ * This software is distributed under the terms of the GNU General Public
+ * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
+ * the license.
+ */
+
 /*
  * Squares3_128.hpp
  *
