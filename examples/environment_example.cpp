fd4f9baf01c684638c51badfdf47ffb32abe51a9
Fan <fan_hu@pku.edu.cn>
2022-03-05 14:41:48 +0800

    add copyright

--- examples/environment_example.cpp
+++ examples/environment_example.cpp
@@ -1,3 +1,11 @@
+/*
+ * (c) Copyright 2022 CORSIKA Project, corsika-project@lists.kit.edu
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
+ * the license.
+ */
+
 #include <corsika/media/Environment.hpp>
 #include <corsika/media/HomogeneousMedium.hpp>
 #include <corsika/media/IMediumModel.hpp>
