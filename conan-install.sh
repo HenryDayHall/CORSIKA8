5edee9ae3521ea56c11236b98d329d508569801d
Felix Riehn <felix@matilda>
2023-02-16 10:34:22 +0100

    hacked cubicspline-boost-fuckup

--- conan-install.sh
+++ conan-install.sh
@@ -9,5 +9,6 @@ if ! conan profile show corsika8 >/dev/null 2>/dev/null; then
     conan profile update settings.compiler.libcxx=libstdc++11 corsika8
   fi
 fi
-
+# force rebuild of cubicinterpolation by removing it first. (see discussion in MR509)
+conan remove cubicinterpolation/* --force
 conan install -pr corsika8 --build=missing ${DIR}
