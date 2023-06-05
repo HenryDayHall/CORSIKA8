34e7fc7951dde887c3006e36b8b92efda9885af2
Nikos Karastathis <n.karastathis@kit.edu>
2023-06-05 17:28:13 +0200

    TabulatedFlatAtmospherePropagator now handles all cases of particles coming from the shower.

--- corsika/modules/radio/propagators/TabulatedFlatAtmospherePropagator.hpp
+++ corsika/modules/radio/propagators/TabulatedFlatAtmospherePropagator.hpp
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2022 CORSIKA Project, corsika-project@lists.kit.edu
+ * (c) Copyright 2023 CORSIKA Project, corsika-project@lists.kit.edu
  *
  * This software is distributed under the terms of the GNU General Public
  * Licence version 3 (GPL Version 3). See file LICENSE for a full version of
@@ -20,7 +20,8 @@ namespace corsika {
    * This class implements a tabulated propagator that approximates
    * the Earth's atmosphere as flat. Signal propagation is rectilinear
    * and this is intended to be used for vertical showers
-   * (<60 degrees zenith angle) for fast simulations.
+   * (<60 degrees zenith angle) for fast simulations. The table needs
+   * to have at least 10 elements.
    *
    */
   template <typename TEnvironment>
@@ -50,24 +51,29 @@ namespace corsika {
     SignalPathCollection propagate(Particle const& particle, Point const& source, Point const& destination);
 
   private:
-    Point const upperLimit_;
-    Point const lowerLimit_;
-    LengthType const step_;
-    InverseLengthType const inverseStep_;
-    std::vector<double> refractivityTable_;
-    std::vector<double> integratedRefractivityTable_;
-    std::vector<LengthType> heightTable_;
-    std::deque<Point> points;
-    std::vector<double> rindex;
+    Point const upperLimit_; ///< the upper point of the table.
+    Point const lowerLimit_; ///< the lowest point of the table (ideally earth's surface).
+    LengthType const minHeight_; ///< z coordinate of lower limit (minimum height) - 1_km for safety reasons.
+    LengthType const maxHeight_; ///< z coordinate of upper limit (maximum height).
+    LengthType const step_; ///< the tabulation step.
+    InverseLengthType const inverseStep_; ///< inverse of the step used to speed up calculations.
+    std::vector<double> refractivityTable_; ///< the table that stores refractivity.
+    std::vector<double> integratedRefractivityTable_; ///< the table that stores integrated refractivity.
+    std::vector<LengthType> heightTable_; ///< the table that stores the height using the step above.
+    std::deque<Point> points; ///< the points that the signal has propagated through.
+    std::vector<double> rindex; ///< the refractive index values along the signal propagation path.
+    double slopeRefrLower_; ///< used to interpolate refractivity for particles below the lowest point.
+    double slopeIntRefrLower_; ///< used to interpolate integrated refractivity for particles below the lowest point.
+    double slopeRefrUpper_; ///< used to interpolate refractivity for particles above the highest point.
+    double slopeIntRefrUpper_; ///< used to interpolate integrated refractivity for particles above the highest point.
+    double lastElement_ ; ///< last index of tables.
 
   }; // End: FlatEarthPropagator
 
   template <typename TEnvironment>
   TabulatedFlatAtmospherePropagator<TEnvironment>
-  make_tabulated_flat_atmosphere_radio_propagator(TEnvironment const& env,
-                                                  Point const& upperLimit,
-                                                  Point const& lowerLimit,
-                                                  LengthType const step) {
+  make_tabulated_flat_atmosphere_radio_propagator(TEnvironment const& env, Point const& upperLimit, Point const& lowerLimit,
+                                                  LengthType const step){
     return TabulatedFlatAtmospherePropagator<TEnvironment>(env, upperLimit, lowerLimit,
                                                            step);
   }
