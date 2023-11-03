0d0b0d434a250783a6f7cc5ab2f5a5d2aa23228d
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2023-11-03 18:36:51 +0100

    adapt sibyll for new RNG

--- modules/sibyll/sibyll2.3d.cpp
+++ modules/sibyll/sibyll2.3d.cpp
@@ -7,10 +7,17 @@
  */
 
 #include <sibyll2.3d.hpp>
+#include <rng_impl.hpp> //from modules/common
 
 #include <cmath>
+#include <array>
+#include <functional>
+#include <iterator>
+#include <iostream>
 
 int get_nwounded() { return s_chist_.nwd; }
 double get_sibyll_mass2(int& id) { return s_mass1_.am2[std::abs(id) - 1]; }
 
-double s_rndm_(int&) { return ::sibyll::rndm_interface(); }
+IMPLEMENT_RNG(sibyll)
+
+double s_rndm_(int&) { return ::draw_std_rnd(); }
