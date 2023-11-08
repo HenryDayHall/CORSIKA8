71d4c875a01f874499863272a47bbe9380bdaf98
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2023-11-08 01:02:56 +0100

    visibility of rmmard_() in CONEX set to hidden
    This remedies a clash of symbols with EPOS, which has its own
    implementation of rmmard_(). For some reason, EPOS would still
    call the CONEX implementation, though.

--- modules/conex/conex_random.cpp
+++ modules/conex/conex_random.cpp
@@ -2,6 +2,7 @@
 
 IMPLEMENT_RNG(conex)
 
-extern "C" void rmmard_(double field[], int const* N, int*) {
+extern "C" __attribute__((visibility("hidden"))) void rmmard_(double field[],
+                                                              int const* N, int*) {
   for (int i = 0; i < *N; ++i) { field[i] = draw_std_rnd(); }
 }
