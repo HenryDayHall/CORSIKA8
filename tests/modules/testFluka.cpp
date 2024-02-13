33f05970901e59d6f7f3031767cb04f1025cc92c
Felix Riehn <friehn@lip.pt>
2024-02-13 20:55:26 +0100

    clang

--- tests/modules/testFluka.cpp
+++ tests/modules/testFluka.cpp
@@ -89,11 +89,11 @@ TEST_CASE("FLUKA") {
   }
 
   SECTION("getCrossSection") {
-    auto const projectileCode = GENERATE(
-        Code::PiMinus, Code::PiMinus, Code::PiMinus, Code::KMinus, Code::K0Long,
-        Code::K0Short, Code::Lambda0, Code::SigmaPlus, Code::Proton, Code::AntiProton,
-        Code::KMinus, Code::K0Long, Code::K0Short, Code::Lambda0, Code::SigmaPlus,
-        Code::Proton, Code::AntiProton);
+    auto const projectileCode =
+        GENERATE(Code::PiMinus, Code::PiMinus, Code::PiMinus, Code::KMinus, Code::K0Long,
+                 Code::K0Short, Code::Lambda0, Code::SigmaPlus, Code::Proton,
+                 Code::AntiProton, Code::KMinus, Code::K0Long, Code::K0Short,
+                 Code::Lambda0, Code::SigmaPlus, Code::Proton, Code::AntiProton);
 
     auto const targetCode = GENERATE(Code::Oxygen, Code::Hydrogen);
 
@@ -127,9 +127,8 @@ TEST_CASE("FLUKA") {
     auto [env, csPtr, nodePtr] = setup::testing::setup_environment(Code::Proton);
     auto const& cs = *csPtr;
 
-    auto const projectileCode =
-        GENERATE(Code::PiPlus, Code::PiMinus, Code::KPlus, Code::K0Long, Code::Lambda0,
-                 Code::SigmaPlus);
+    auto const projectileCode = GENERATE(Code::PiPlus, Code::PiMinus, Code::KPlus,
+                                         Code::K0Long, Code::Lambda0, Code::SigmaPlus);
     auto const p = GENERATE(1_GeV, 20_GeV, 100_GeV, 1_TeV);
     auto [stackPtr, secViewPtr] = setup::testing::setup_stack(
         Code::Hydrogen, 1_GeV, (DummyEnvironment::BaseNodeType* const)nodePtr, *csPtr);
