b54a14739afdd20efb928c04805ab6657200b3c2
Marvin Gottowik <marvingottowik@web.de>
2024-12-17 15:39:38 +0100

    update license info in file header

--- src/modules/sophia/code_generator.py
+++ src/modules/sophia/code_generator.py
@@ -4,9 +4,8 @@
 #
 # See file AUTHORS for a list of contributors.
 #
-# This software is distributed under the terms of the GNU General Public
-# Licence version 3 (GPL Version 3). See file LICENSE for a full version of
-# the license.
+# This software is distributed under the terms of the 3-clause BSD license.
+# See file LICENSE for a full version of the license.
 
 
 import pickle, sys, itertools
