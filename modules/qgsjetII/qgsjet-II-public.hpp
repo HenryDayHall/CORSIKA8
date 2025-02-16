517fdeffcff45a4dcf007c525affad58dc11a10d
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2025-02-17 00:51:00 +0100

    make pointers const

--- modules/qgsjetII/qgsjet-II-public.hpp
+++ modules/qgsjetII/qgsjet-II-public.hpp
@@ -5,22 +5,18 @@
 #include "qgsjet-II-04-types.hpp"
 
 namespace QGSJetII04 {
+  extern QGARR12* const qgarr12_;
+  extern QGARR13* const qgarr13_;
+  extern QGARR14* const qgarr14_;
+  extern QGARR55* const qgarr55_;
 
-  //~ int const* nptmax;
-  //~ int const* iapmax;
+  // function pointers to internal (FORTRAN) functions
+  extern void (*const qgset_)();
+  extern void (*const qgaini_)(char const*);
+  extern void (*const qgini_)(double const&, int const&, int const&, int const&);
+  extern void (*const qgconf_)();
+  extern double (*const qgsect_)(double const&, int const&, int const&, int const&);
+  extern double (*const qgran_)(int&);
 
-  extern QGARR12* qgarr12_;
-  extern QGARR13* qgarr13_;
-  extern QGARR14* qgarr14_;
-  extern QGARR55* qgarr55_;
-
-  // function pointers to internal (FORTRAN) symbols
-  extern void (*qgset_)();
-  extern void (*qgaini_)(char const*);
-  extern void (*qgini_)(double const&, int const&, int const&, int const&);
-  extern void (*qgconf_)();
-  extern double (*qgsect_)(double const&, int const&, int const&, int const&);
-  extern double (*qgran_)(int&);
-
-  extern void (*set_rng_function)(std::function<void(double*, std::size_t)>);
+  extern void (*const set_rng_function)(std::function<void(double*, std::size_t)>);
 } // namespace QGSJetII04
