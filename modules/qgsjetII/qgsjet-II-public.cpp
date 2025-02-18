ee5d050ba0ee7296fec4fc35f88d5cb600f00e3c
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2025-02-18 09:25:15 +0100

    fix extern/const issue

--- modules/qgsjetII/qgsjet-II-public.cpp
+++ modules/qgsjetII/qgsjet-II-public.cpp
@@ -4,19 +4,21 @@
 #include "qgsjet-II-04-private.hpp"
 
 namespace QGSJetII04 {
-  QGARR12* const qgarr12_ = &::qgarr12_;
-  QGARR14* const qgarr14_ = &::qgarr14_;
-  QGARR13* const qgarr13_ = &::qgarr13_;
-  QGARR55* const qgarr55_ = &::qgarr55_;
+  extern QGARR12* const qgarr12_ = &::qgarr12_;
+  extern QGARR14* const qgarr14_ = &::qgarr14_;
+  extern QGARR13* const qgarr13_ = &::qgarr13_;
+  extern QGARR55* const qgarr55_ = &::qgarr55_;
 
   // functions
-  void (*const qgset_)() = &::qgset_;
-  void (*const qgaini_)(char const*) = &::qgaini_;
-  void (*const qgini_)(double const&, int const&, int const&, int const&) = &::qgini_;
-  void (*const qgconf_)() = &::qgconf_;
-  double (*const qgsect_)(double const&, int const&, int const&, int const&) = &::qgsect_;
-  double (*const qgran_)(int&) = &::qgran_;
+  extern void (*const qgset_)() = &::qgset_;
+  extern void (*const qgaini_)(char const*) = &::qgaini_;
+  extern void (*const qgini_)(double const&, int const&, int const&,
+                              int const&) = &::qgini_;
+  extern void (*const qgconf_)() = &::qgconf_;
+  extern double (*const qgsect_)(double const&, int const&, int const&,
+                                 int const&) = &::qgsect_;
+  extern double (*const qgran_)(int&) = &::qgran_;
 
-  void (*set_rng_function)(std::function<void(double*, std::size_t)>) =
+  extern void (*const set_rng_function)(std::function<void(double*, std::size_t)>) =
       &::qgsjetII::set_rng_function;
 } // namespace QGSJetII04
