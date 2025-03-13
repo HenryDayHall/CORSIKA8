00c78b0afd41b63e648e1e1c874d7368d98095d4
Alice Faure <alice.faure@umontpellier.fr>
2025-03-13 16:06:28 +0000

    Edit testOnShellCheck.cpp

--- tests/modules/testOnShellCheck.cpp
+++ tests/modules/testOnShellCheck.cpp
@@ -57,7 +57,7 @@ TEST_CASE("OnShellCheck", "[processes]") {
     // add secondaries, all with energies above the threshold
     // only cut is by species
     int count = -1;
-    for (auto const& proType : particleList) {
+    for (auto const proType : particleList) {
       count++;
       const auto pz = sqrt((E - get_mass(proType) * mass_shifts[count]) *
                            (E + get_mass(proType) * mass_shifts[count]));
