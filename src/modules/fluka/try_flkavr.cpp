a407cd28e4a2fa601144560c8ee9f1393d0ff6d2
Marvin Gottowik <marvingottowik@web.de>
2025-09-04 14:02:44 +0200

    copyright and format

--- src/modules/fluka/try_flkavr.cpp
+++ src/modules/fluka/try_flkavr.cpp
@@ -1,14 +1,17 @@
+/*
+ * (c) Copyright YEAR CORSIKA Project, corsika-project@lists.kit.edu
+ *
+ * This software is distributed under the terms of the 3-clause BSD license.
+ * See file LICENSE for a full version of the license.
+ */
+
 #include <stdio.h>
 #include <ctype.h>
 
 extern "C" {
-extern struct {
-  int mjflvr, mnflvr, mrflvr;
-} flkavr_;
+extern struct { int mjflvr, mnflvr, mrflvr; } flkavr_;
 
-extern struct {
-  char chflvr;
-} flkavc_;
+extern struct { char chflvr; } flkavc_;
 }
 
 int main() {
