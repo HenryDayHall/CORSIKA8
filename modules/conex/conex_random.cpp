06d932941688aeaac6ed008995e29a3f9914e112
Maximilian Reininghaus <maximilian.reininghaus@tu-dortmund.de>
2025-10-07 16:02:56 +0200

    re-enable cxroot, implement missing CONEX functions

--- modules/conex/conex_random.cpp
+++ modules/conex/conex_random.cpp
@@ -6,3 +6,11 @@ extern "C" __attribute__((visibility("hidden"))) void rmmard_(double field[],
                                                               int const* N, int*) {
   for (int i = 0; i < *N; ++i) { field[i] = draw_std_rnd(); }
 }
+
+extern "C" __attribute__((visibility("hidden"))) double drangen_() { return draw_std_rnd(); }
+
+// this is needed as linker object, but it is not needed to do anything
+extern "C" __attribute__((visibility("hidden"))) void rmmaqd_(int[3], int&, char*, int) {}
+
+// this is needed as linker object, but it is not needed to do anything
+extern "C" __attribute__((visibility("hidden"))) void ranfgt_(int&) {}
