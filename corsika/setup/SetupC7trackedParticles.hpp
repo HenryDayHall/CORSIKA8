5ec3d997ddf1b18083c6e0946b0575dc108b686f
Felix Riehn <friehn@lip.pt>
2025-12-17 10:21:31 +0000

    Resolve "Include (anti-)lambda for interactions in Pythia 8/Angantyr"

--- corsika/setup/SetupC7trackedParticles.hpp
+++ corsika/setup/SetupC7trackedParticles.hpp
@@ -21,4 +21,25 @@ namespace corsika::setup {
       Code::LambdaBar,     Code::SigmaPlus, Code::SigmaPlusBar, Code::SigmaMinus,
       Code::SigmaMinusBar, Code::Xi0,       Code::Xi0Bar,       Code::OmegaMinus,
       Code::OmegaPlusBar,  Code::MuPlus,    Code::MuMinus};
+
+  std::set<Code> C7trackedParticlesAndCharm{
+      Code::Proton,         Code::Neutron,   Code::AntiProton,   Code::AntiNeutron,
+      Code::PiPlus,         Code::PiMinus,   Code::Pi0,          Code::KPlus,
+      Code::KMinus,         Code::K0Long,    Code::K0Short,      Code::Lambda,
+      Code::LambdaBar,      Code::SigmaPlus, Code::SigmaPlusBar, Code::SigmaMinus,
+      Code::SigmaMinusBar,  Code::Xi0,       Code::Xi0Bar,       Code::OmegaMinus,
+      Code::OmegaPlusBar,   Code::MuPlus,    Code::MuMinus,      Code::D0,
+      Code::D0Bar,          Code::DPlus,     Code::DMinus,       Code::LambdaCPlus,
+      Code::LambdaCMinusBar};
+
+  std::set<Code> C7trackedParticlesAndBeauty{
+      Code::Proton,          Code::Neutron,   Code::AntiProton,   Code::AntiNeutron,
+      Code::PiPlus,          Code::PiMinus,   Code::Pi0,          Code::KPlus,
+      Code::KMinus,          Code::K0Long,    Code::K0Short,      Code::Lambda,
+      Code::LambdaBar,       Code::SigmaPlus, Code::SigmaPlusBar, Code::SigmaMinus,
+      Code::SigmaMinusBar,   Code::Xi0,       Code::Xi0Bar,       Code::OmegaMinus,
+      Code::OmegaPlusBar,    Code::MuPlus,    Code::MuMinus,      Code::D0,
+      Code::D0Bar,           Code::DPlus,     Code::DMinus,       Code::LambdaCPlus,
+      Code::LambdaCMinusBar, Code::B0,        Code::B0Bar,        Code::BPlus,
+      Code::BMinus};
 } // namespace corsika::setup
