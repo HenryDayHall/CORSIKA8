c4f631d13ee5fddcace2a9f85e025759d44ebd77
Henry Day-Hall <henry.day-hall@desy.de>
2026-03-04 17:30:13 +0100

    Additions to track a history

---
+++ corsika/modules/writers/HistoryWriter.hpp
@@ -0,0 +1,66 @@
+/*
+ * (c) Copyright 2024 CORSIKA Project, corsika-project@lists.kit.edu
+ *
+ * This software is distributed under the terms of the 3-clause BSD license.
+ * See file LICENSE for a full version of the license.
+ */
+
+#pragma once
+
+#include <corsika/framework/process/SecondariesProcess.hpp>
+
+#include <corsika/media/ShowerAxis.hpp>
+
+#include <corsika/modules/ObservationPlane.hpp>
+
+#include <corsika/output/BaseOutput.hpp>
+#include <corsika/output/ParquetStreamer.hpp>
+
+namespace corsika {
+
+  template <typename TTracking, typename TOutput>
+  class HistoryWriter
+      : public SecondariesProcess<HistoryWriter<TTracking, TOutput>>,
+        public BaseOutput {
+
+  public:
+    /**
+     *
+     * @param axis - axis used for calculating slant depth of interaction
+     * @param obsPlane - used to define the location of the particles
+     */
+    HistoryWriter(media::ShowerAxis const& axis,
+                      ObservationPlane<TTracking, TOutput> const& obsPlane);
+
+    /**
+     *
+     * @tparam TStackView
+     */
+    template <typename TStackView>
+    void doSecondaries(TStackView&);
+
+    void startOfLibrary(boost::filesystem::path const&) override;
+    void endOfLibrary() override;
+
+    void startOfShower(unsigned int const) override;
+    void endOfShower(unsigned int const) override;
+
+    YAML::Node getConfig() const override;
+    YAML::Node getSummary() const override;
+
+
+  private:
+    ObservationPlane<TTracking, TOutput> const obsPlane_;
+    media::ShowerAxis const& showerAxis_;
+
+    unsigned int nextUnusedLabel_;
+    unsigned int showerId_;
+
+    ParquetStreamer output_;
+    YAML::Node summary_;
+
+  }; // namespace corsika
+
+} // namespace corsika
+
+#include <corsika/detail/modules/writers/HistoryWriter.inl>
